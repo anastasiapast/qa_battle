ca51ae85f5dc2238dfc575a3a6e1d12c40bf2792
Anastasia <anastasiapast@live.com>
2019-12-03 22:17:10 +0300

    commit

--- src/test/java/com/propellerads/qa/pages/login/LoginPage.java
+++ src/test/java/com/propellerads/qa/pages/login/LoginPage.java
@@ -4,27 +4,47 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.propellerads.qa.pages.base.BasePage;
+import lombok.Getter;
+import lombok.Setter;
 
+import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
+@Getter
+@Setter
 public class LoginPage extends BasePage {
 
-    public SelenideElement loginInput = $("#loginInput");
+    private SelenideElement loginInput = $("#loginInput");
 
-    public SelenideElement passwordInput = $("#passwordInput");
+    private SelenideElement passwordInput = $("#passwordInput");
 
-    public SelenideElement formInput = $$(".form-group").get(0);
+    private SelenideElement formInputLogin = $$(".form-group").findBy(attribute("onclick", "startInputLogin()"));
 
-    public SelenideElement hoverButton = $$(".btn-primary").findBy(text("Hover me faster!"));
+    private SelenideElement formInputPassword = $$(".form-group").findBy(attribute("onclick", "startInputPassword()"));
 
-    public SelenideElement waitButton = $$(".btn-primary").findBy(text("Wait for some time..."));
+    private SelenideElement hoverButton = $$(".btn-primary").findBy(text("Hover me faster!"));
 
-    public SelenideElement signIn = $(".card-footer img");
+    private SelenideElement waitButton = $$(".btn-primary").findBy(text("Wait for some time..."));
+
+    private SelenideElement signIn = $(".card-footer img");
 
     public LoginPage open() {
         Selenide.open(Configuration.baseUrl + "/index.html");
         return this;
     }
+
+    public LoginPage login(String user, String password) {
+        formInputLogin.click();
+        loginInput.click();
+        loginInput.clear();
+        loginInput.setValue(user);
+        formInputPassword.click();
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.setValue(password);
+        return this;
+    }
+
 }
--- src/test/java/com/propellerads/qa/tests/login/LoginTest.java
+++ src/test/java/com/propellerads/qa/tests/login/LoginTest.java
@@ -1,9 +1,15 @@
 package com.propellerads.qa.tests.login;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
 import com.propellerads.qa.pages.login.LoginPage;
 import com.propellerads.qa.tests.base.BaseTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.visible;
+
 
 public class LoginTest extends BaseTest {
 
@@ -13,15 +19,23 @@ public class LoginTest extends BaseTest {
     @Test
     void correctLogin(){
         loginPage.open();
-        loginPage.formInput.click();
-        loginPage.loginInput.click();
-        loginPage.loginInput.setValue("test");
-        loginPage.passwordInput.setValue("test");
+        loginPage.login("test", "test");
+        loginPage.getHoverButton().shouldBe(visible.because("'Hover me faster' button is not visible")).hover();
+        loginPage.getWaitButton().waitUntil(appear, 5000);
+        loginPage.getSignIn().waitUntil(appear, 10000);
+        loginPage.getSignIn().shouldBe(visible.because("'Sign In' button is not visible")).click();
+
+        //wait until alert - https://seleniumatfingertips.wordpress.com/2016/07/05/check-whether-alert-is-present-on-the-webpage-or-not/
+        WebDriverRunner.getWebDriver().switchTo().alert().accept();
+        WebDriverRunner.getWebDriver().switchTo().alert().accept();
+
+        int a = 10;
 
         //https://medium.com/@rosolko/fast-authorization-level-local-storage-6c84e9b3cef1
         //https://medium.com/@rosolko/boost-you-autotests-with-fast-authorization-b3eee52ecc19
         //https://medium.com/@rosolko/simple-allure-2-configuration-for-gradle-8cd3810658dd
     }
+
 
 
 }
